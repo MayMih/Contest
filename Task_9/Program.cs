1ce82d5303d6e4092fd9ef24c0b5d7b230be3f6d
Mikhail M <mayurovmih@gmail.com>
2022-10-26 21:10:14 +0300

    Решил 9-ю задачу, но похоже у них ошибка в условии, т.к. их же проверочный пример не проходит.

---
+++ Task_9/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task_9
+{
+    /// <summary>
+    /// Задача 9. "Обеды в кафе" (минимакс)
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Граничная стоимость одного обеда, за превышение которой дают купон на бесплатный обед.
+        /// </summary>
+        public const uint FREE_DINNER_BOUNDARY = 100;
+
+        static void Main()
+        {
+            Console.WriteLine("Введите кол-во дней, на которые запланированы обеды:");
+            uint daysCount = uint.Parse(Console.ReadLine().Trim());
+            if (daysCount > 100)
+            {
+                throw new ArgumentOutOfRangeException("В первой строке должно быть натуральное число ﻿﻿(0 ≤ n ≤ 100)");
+            }
+            Console.WriteLine($"Введите {daysCount} строк со стоимостью обеда в этот день:");
+            var dinnerCosts = new List<uint>();
+            while (dinnerCosts.Count < daysCount)
+            {
+                var cost = uint.Parse(Console.ReadLine().Trim());
+                if (cost > 300)
+                {
+                    throw new ArgumentOutOfRangeException("Стоимость обеда должна быть неотрицательным целым числом ≤ 300!");
+                }
+                dinnerCosts.Add(cost);
+            }
+            var minSumCost = GetMinSumCost(dinnerCosts.AsReadOnly(), FREE_DINNER_BOUNDARY);
+            Console.WriteLine(minSumCost);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод получения минимально возможной суммарной стоимости обедов (с учётом купонов) за указанное кол-во дней.
+        /// </summary>
+        public static uint GetMinSumCost(IReadOnlyList<uint> dinnerCosts, uint freeDinnerBoundary)
+        {
+            if (dinnerCosts.Count  == 0)
+            {
+                return 0;
+            }
+            uint minSumCost = 0;
+            var ignoredElements = new List<uint>();
+            for (int i = 0; i < dinnerCosts.Count; i++)
+            {
+                if (ignoredElements.Contains((uint)i))
+                {
+                    continue;
+                }
+                minSumCost += dinnerCosts[i];                
+                if (dinnerCosts[i] > freeDinnerBoundary)
+                {
+                    uint localMaxIndex = GetMaxElementIndex(dinnerCosts, i + 1);
+                    ignoredElements.Add(localMaxIndex);
+                }
+            }
+            return minSumCost;
+        }
+
+        /// <summary>
+        /// Метод поиска максимального элемента в коллекции начиная с указанной позиции
+        /// </summary>
+        /// <param name="dinnerCosts"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static uint GetMaxElementIndex(IReadOnlyList<uint> dinnerCosts, int startIndex)
+        {
+            if ((dinnerCosts?.Count ?? 0) == 0)
+            {
+                throw new ArgumentException("Список не может быть пустым!");
+            }
+            if (startIndex < 0 || startIndex >= dinnerCosts.Count)
+            {
+                throw new ArgumentOutOfRangeException($"Параметр {nameof(startIndex)} должен быть меньше {dinnerCosts.Count}!");
+            }
+            int maxIndex = startIndex;
+            var max = dinnerCosts[maxIndex];
+            for (int i = startIndex; i < dinnerCosts.Count; i++)
+            {
+                var elem = dinnerCosts[i];
+                if (elem > max)
+                {
+                    max = elem;
+                    maxIndex = i;
+                }
+            }
+            return (uint)maxIndex;
+        }
+    }
+}
---
+++ Task_9_Tests/Program_Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using NUnit.Framework;
+
+
+namespace Task_9.Tests
+{
+    [TestFixture(TestName = "Основной метод - поиск минимальной суммы покупок с учётом купонов")]
+    public class GetMinSumCost_Tests
+    {
+        [TestCase(new uint[] { 5, 35, 40, 101, 59, 63 }, ExpectedResult = 240)]
+        public uint GetMinSumCost_NormalTest(uint[] numbers)
+        {
+            return Program.GetMinSumCost(numbers, Program.FREE_DINNER_BOUNDARY);
+        }
+
+        
+    }
+
+    [TestFixture(TestName = "Вспомогательный метод - локальный максимум")]
+    public class GetMaxElementIndex_Tests
+    {
+        [TestCase(new uint[] { 5, 35, 40, 101, 59, 63 }, 3, ExpectedResult = 101)]
+        [TestCase(new uint[] { 5, 35, 40, 101, 59, 63 }, 4, ExpectedResult = 63)]
+        [TestCase(new uint[] { 5, 35, 40, 101, 59, 63 }, 2, ExpectedResult = 101)]
+        [TestCase(new uint[] { 5, 101, 35, 63, 40, 59 }, 2, ExpectedResult = 63)]
+        [TestCase(new uint[] { 5, 101, 35, 63, 40, 59 }, 1, ExpectedResult = 101)]
+        [TestCase(new uint[] { 5, 101, 35, 63, 40, 59 }, 4, ExpectedResult = 59)]
+        [TestCase(new uint[] { 5, 35 }, 1, ExpectedResult = 35)]
+        [TestCase(new uint[] { 5, 35 }, 0, ExpectedResult = 35)]
+        [TestCase(new uint[] { 3 }, 0, ExpectedResult = 3)]
+        [Test]
+        public uint GetMaxElementIndex_PredefinedNormalTest(uint[] numbers, int startIndex)
+        {
+            return numbers[Program.GetMaxElementIndex(numbers, startIndex)];
+        }
+        
+        [TestCase(new uint[] { 5 }, 1)]
+        [TestCase(new uint[] { 5, 6 }, -5)]
+        [Test]
+        public void GetMaxElementIndex_IllegalArgTest(uint[] numbers, int startIndex)
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => Program.GetMaxElementIndex(numbers, startIndex));
+        }
+        
+        [TestCase(null, 1)]
+        [TestCase(new uint[0], 5)]
+        [Test]
+        public void GetMaxElementIndex_EmptyListTest(uint[] numbers, int startIndex)
+        {
+            Assert.Throws(typeof(ArgumentException), () => Program.GetMaxElementIndex(numbers, startIndex));
+        }
+    }
+}
